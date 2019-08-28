a0badfc703b7333145c24b146de6b9c35c400f8d
Annie Truex <artruex@gmail.com>
2019-08-28 01:20:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,11 +18,14 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
